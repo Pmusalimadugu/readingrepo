b86cddfe58dc77ee7338a6cf977fa3d0c3c099b0
Pranay M <pmusalimadugu@gmail.com>
2022-04-15 20:42:37 -0400

    stole code from the internet?

--- frontend/readingrepo-widget/src/index.js
+++ frontend/readingrepo-widget/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// Find all widgetDivs
+const widgetDivs = document.querySelectorAll('.book_widget')
+
+// Inject our React App into each
+widgetDivs.forEach(Div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div}/>
+    </React.StrictMode>,
+    Div
+  );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
